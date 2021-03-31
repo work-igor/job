22155b39cc173cc8930f5efbb70a9386f87099c1
work-igor <76725591+work-igor@users.noreply.github.com>
2021-03-31 11:48:51 +0300

    Update phone template

--- wp-content/themes/generatepress-child/templates/single-vacan.php
+++ wp-content/themes/generatepress-child/templates/single-vacan.php
@@ -35,32 +35,11 @@ get_header(); ?>
 				<div class="emp__item"><img src="<?php the_field('employment_img'); ?>" alt=""></div>
 				<div class="emp__item emp__item-mobile"><img src="<?php the_field('employment_img_mobile'); ?>" alt=""></div>
 			</div>
-			<div>
-				<div class="posts-nav">
-					<?php if ( has_post_thumbnail() && ! post_password_required() && ! is_attachment()  ) : ?>
-						<?php
-							$nextPost = get_next_post();
-							if( ! empty($nextPost) ){
-									$nextthumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $nextPost->ID ), 'thumbnail' );?>
-
-							<div class="postNavigation nextPostBox"><?php next_post_link('%link','<div class="inner-prevnext"><div class="thumbnail"><img src="' . esc_url( $nextthumbnail[0] ) . '"  alt="%title"/></div><div class="headline"><span>'. __('', 'nanima') .'</span><h4>%title</h4></div></div>'); ?></div>
-							<?php }
-							?>
-						<?php
-							$prevPost = get_previous_post();
-							if( ! empty($prevPost) ){
-								$prevthumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $prevPost->ID ), 'thumbnail' );?>
-
-							<div class="postNavigation prevPostBox"><?php previous_post_link('%link','<div class="inner-prevnext"><div class="thumbnail"><img src="' . esc_url( $prevthumbnail[0] ) . '"/></div><div class="headline"><span>'. __('', 'nanima') .'</span><h4>%title</h4></div></div>'); ?></div>
-							<?php }
-							?>
-					<?php endif; ?>
-				</div><!-- navigation End -->
-			</div>
 		</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 	</div>
+
 	<?php
 
 
